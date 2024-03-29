492aa96947c0177171b39d2763afcbb52e68f31f
Austin <austinngobeni2002@gmail.com>
2024-03-29 18:08:38 +0200

    wrote view file tests

--- tests/TestViewFile.py
+++ tests/TestViewFile.py
@@ -1,10 +1,5 @@
 import unittest
-import main
-import text_operations
-import text_operations.removeFile as rm
 import text_operations.viewFile as vf
-import text_operations.writeFile as wf
-import text_operations.search_file as sf
 import os
 
 from unittest.mock import patch
@@ -20,7 +15,20 @@ class TestViewClass(unittest.TestCase):
 	def tearDown(self):
 		os.remove(self.file_path)
 
-	def test_view_file_exists(self):		
+
+	@patch("builtins.print")
+	def test_view_file_exists(self, mock_print):		
 		output = vf.show_file_contents(True, self.file_path)
-		self.assertEqual("""If you are fan of the strange dark and mysterious delivered in story format, take
-the like button skydiving but give it a faulty parachute....""", output)
+		mock_print.assert_called_once_with("""If you are fan of the strange dark and mysterious delivered in story format, take
+the like button skydiving but give it a faulty parachute....""")
+
+
+	@patch("builtins.print")
+	def test_view_file_doesnt_exist(self, mock_print):
+		output = vf.show_file_contents(False, self.file_path + "e")
+		mock_print.assert_called_once_with("Cannot open the file, make sure it exists.")
+
+
+
+if __name__ == "__main__":
+	unittest.main()
