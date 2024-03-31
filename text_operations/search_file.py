99c5c6d28e89feafa7bb367e0a036f3ee5dd6525
Austin <austinngobeni2002@gmail.com>
2024-03-31 14:09:01 +0200

    remove file

--- text_operations/search_file.py
+++ text_operations/search_file.py
@@ -1,12 +1,13 @@
 import sys, os
+from halo import Halo
 
 def take_filename():
 	return input("Enter file name (filename must have file extension): ")
 	
 
+@Halo(text="This may take a while...Super highly advanced virtual bots are searching for your file :)")
 def find_file(file_name, search_path):
 	for root, dirs, files in os.walk(search_path):
 		if file_name in files:
 			return True, os.path.join(root, file_name)
 	return False, None
-
---
+++ text_operations/writeFile.py
@@ -0,0 +1,7 @@
+import sys, os
+sys.path.append(os.getcwd())
+import text_operations.search_file as sf
+
+
+def write_to_file():
+	pass
