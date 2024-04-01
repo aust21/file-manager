0a4ccd80cde5cb998fb20aca4f0999efd0cc4e54
Austin <austinngobeni2002@gmail.com>
2024-04-01 18:43:47 +0200

    add: add pdf class

---
+++ text_operations/pdf_handler.py
@@ -0,0 +1,26 @@
+from fpdf import FPDF
+
+class PDF(FPDF):
+	def header(self, title):
+		self.set_font("times", "", 14)
+		title_width = self.get_string_width(title)
+		doc_width = self.w
+		self.set_x((doc_width - title_width) // 2)
+		self.set_fill_color(0, 80, 180)
+		self.set_text_color(220, 50, 50)
+		self.set_line_width(1)
+		self.cell(title_w, 10, title, border=False, align="C")
+		self.ln(14)
+
+
+	def chapter_body(self, name):
+		with open(name, "rb") as fh:
+			txt = fh.read().decode("latin-1")
+
+		self.set_font("times", "", 12)
+		self.multi_cell(0, 5, txt)
+		self.ln()
+
+# document = PDF("P", "mm", "Letter")
+# document.set_auto_page_break(auto = True, margin = 15)
+# document_header = document.header
--- text_operations/writeFile.py
+++ text_operations/writeFile.py
@@ -1,6 +1,8 @@
 import sys, os
+from fpdf import FPDF
 sys.path.append(os.getcwd())
 import text_operations.search_file as sf
+import text_operations.pdf_hander as pdf
 
 def create_txt_read_file(file_name, file_type, content):
 	pass
