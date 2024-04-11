ebf0ddce1c697be5def7842ca0cf73d93939a383
Austin <austinngobeni2002@gmail.com>
2024-04-11 18:56:29 +0200

    feat: add feature to create a file to according to the file prefix name

--- text_operations/writeFile.py
+++ text_operations/writeFile.py
@@ -13,8 +13,14 @@ def create_file_manager_dir(path) -> None:
 		os.mkdir(path)
 
 
-def create_txt_read_file(file_name, file_type, path) -> None:
-	with open(f"{path}\\{file_name.split('.')[0]}.txt", 'w') as fl:
+def create_sub_directory(file_name, path):
+	if not os.path.exists(f"{path}\\{file_name[:3]}"):
+		os.mkdir(f"{path}\\{file_name[:3]}")
+	return file_name[3:], f"{path}\\{file_name[:3]}"
+
+
+def create_txt_read_file(file_name, file_type, sub_dir) -> None:
+	with open(f"{sub_dir}\\{file_name.split('.')[0]}.txt", 'w') as fl:
 		print("Enter the file contents (Press enter on an empty line when you are done)")
 		while True:
 			content = input()
@@ -27,25 +33,28 @@ def extract_extension(file_name) -> str:
 	return file_name.split(".")[1]
 
 
-def write_to_file(valid_name, content, path) -> None:
-	if not valid_name:
+def write_to_file(file_name, content, sub_dir) -> None:
+	print(file_name)
+	if not file_name:
 		print("Invalid file name.")
 	else:
 		create_file_body = pdf.PDF("P", "mm", "Letter")
 		create_file_body.set_auto_page_break(auto = True, margin = 15)
 		create_file_body.add_page()
-		create_file_body.chapter_body(f"{path}\\{valid_name[:-4]}.txt")
-		create_file_body.output(f"{path}\\{valid_name}")
+		create_file_body.chapter_body(f"{sub_dir}\\{file_name[:-4]}.txt")
+		create_file_body.output(f"{sub_dir}\\{file_name}")
 		print("File created")
 
 
 def main(valid_name) -> None:
 	path = set_path()
+	file_name, sub_dir = create_sub_directory(valid_name, path)
 	create_file_manager_dir(path)
+	# print(f"final name{file_name}\n\n\n\n\n")
 	file_type = extract_extension(file_name)
-	text_format = create_txt_read_file(file_name, file_type, path)
+	text_format = create_txt_read_file(file_name, file_type, sub_dir)
 	if file_type == "pdf":
-		write_to_file(valid_name, f"{path}\\{valid_name.split('.')[0]}.txt", path)
+		write_to_file(file_name, f"{sub_dir}\\{file_name.split('.')[0]}.txt", sub_dir)
 
 
 if __name__ == "__main__":
