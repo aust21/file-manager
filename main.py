6d8a0e1b523b66bd46e5959666a8091db67e07d2
Austin <austinngobeni2002@gmail.com>
2024-04-25 14:13:52 +0200

    add layout - tests need to update

--- main.py
+++ main.py
@@ -2,23 +2,45 @@ from rich import print
 from rich.console import Console
 from rich.panel import Panel
 from rich.table import Table
+from rich.layout import Layout
 
-def welcome(title, subtitle, message):
-	print(Panel(message, title=title, subtitle=subtitle))
+layout = Layout()
+console = Console(height = 25)
 
-def commands():
-	return """Command                       Action
--------------------------------------------------------------------
--> open <filename>                  opens file
--> create <filename>                creates and write to a new file
--> read <filename>                  reads out file contents
--------------------------------------------------------------------
--------------------------------------------------------------------"""
+def app_layout(title, subtitle, message, commands, actions):
+	layout.split_column(
+		Layout(Panel(message, title="File Manager", subtitle="By Austin")),
+		Layout(name = "bottom"),
+	)
+
+	layout["bottom"].split_row(
+	    Layout(Panel(commands, title="Commands")),
+	    Layout(Panel(actions, title="Action")),
+	)
+	# layout["top"].size = 5
+	layout["bottom"].size = 20
+	console.print(layout)
+
+def file_actions():
+	return "actions"
+
+
+def valid_commands():
+	return "commands"
+# 	return """Command                       Action
+# -------------------------------------------------------------------
+# -> open <filename>                  opens file
+# -> create <filename>                creates and write to a new file
+# -> read <filename>                  reads out file contents
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------"""
 
 
 if __name__ == "__main__":
 	message = "\nWelcome to file manager, the all you need file management tool for easy file operations\n"
 	title = "File Manager"
 	subtitle = "Thank you for choosing File Manager"
-	welcome(title, subtitle, message)
-	# print(commands())
+	commands = valid_commands()
+	actions = file_actions()
+	app_layout(title, subtitle, message, commands, actions)
+	# commands()
