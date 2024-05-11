3303441a341642f0d561667da2814e9ded49c42a
Austin <austinngobeni2002@gmail.com>
2024-05-11 19:20:31 +0200

    add tqdm progress

--- text_operations/search_file.py
+++ text_operations/search_file.py
@@ -1,10 +1,11 @@
-import sys, os
-import subprocess
-import time
+import sys, os, time, subprocess
 from halo import Halo
 from rich.console import Console
 from rich.panel import Panel
 from rich.layout import Layout
+from threading import Thread
+from rich.progress import Progress, BarColumn
+from tqdm import tqdm
 
 layout = Layout()
 console = Console(height = 25)
@@ -44,20 +45,34 @@ def file_input_instructions() -> str:
 		"The second bit `myinvoice` is the name of the file\nThe last bit `.pdf` is the file type."
 
 
+def search_instructions() -> str:
+	return "Enter file name below. If the file is found a path to the file will be "\
+		"displayed and the file explorer will be automatically opened."
+
+
+def discover_file_number(dir):
+	count = 0
+	for file in tqdm(os.listdir(dir)):
+		if os.path.isfile(os.path.join(dir, file)):
+			count += 1
+	return count
+
+
 def find_file(file_name, search_path) -> tuple[bool, str]:
-	for root, dirs, files in os.walk(search_path):
+	for root, dirs, files in tqdm(os.walk(search_path), desc="Searching"):
 		if file_name in files:
 			return True, os.path.join(root, file_name)
 	return False, ""
 
 
-
 if __name__ == "__main__":
-	instructions = file_input_instructions()
+	instructions = search_instructions()
 	display_messages(instructions)
 	file_name = take_filename()
-	find_path = find_file(file_name, "C:\\")
+	find_path = find_file(file_name, "C:\\Users\\Austin\\Desktop\\")
 	if find_path[0]:
 		print("Opening explorer")
 		time.sleep(3)
 		subprocess.Popen(fr'explorer /select,"{find_path[1]}"')
+	else:
+		print("File not found, please make sure it exists")
