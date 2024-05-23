4ab47931fa80946b9886d3c5730c19d1f0365762
Austin <austinngobeni2002@gmail.com>
2024-05-23 17:29:04 +0200

    add panel for view file

--- text_operations/viewFile.py
+++ text_operations/viewFile.py
@@ -1,13 +1,24 @@
 import sys, os
 sys.path.append(os.getcwd())
+from modules import *
 import text_operations.search_file as sf
 
+
+layout = Layout()
+# console = Console(height = 12)
+
+
+def display_content(text, file_name):
+	layout.split_column(
+		Layout(Panel(text, title=file_name))
+	)
 
 def show_file_contents(file_exists, file_path) -> None:
 	if file_exists:
 		with open(file_path, "r") as fl:
 			content = fl.readlines()
-			print("".join(content))
+			display_content("".join(content), file_name)
+			console.print(layout)
 	else:
 		print("Cannot open the file, make sure it exists.")
 
